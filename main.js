ba949e275ac3173e4524212b6e60a6f11f9880f8
Alejandro <alejmac2508@gmail.com>
2024-07-04 23:40:44 -0600

    se agregaron funciones para el manejos de los eventos

--- main.js
+++ main.js
@@ -3,4 +3,20 @@ let time = 0;
 //nueva funcion 
 let timer = null;
 let timerbrake = null ;
-let current
+let current = null;
+
+const bAdd = document.querySelector('#bAdd');
+const itTask = document.querySelector('#itTask');
+const form = document.querySelector('#bAdd');
+
+form.addEventListener('submit',e =>{
+    e.preventDefault();
+    if (itTask.value != ''){
+        createTask(itTask.value);
+        itTask.value ='';
+    };
+});
+//contiene elementeos y objetos
+function createTask(value){
+    const newTask ="";
+}
