46a875c2b2970b14d6a4bc1121224281db4e66bd
Alejandro <alejmac2508@gmail.com>
2024-07-05 00:00:13 -0600

    metodo para convertir en html

--- main.js
+++ main.js
@@ -9,14 +9,34 @@ const bAdd = document.querySelector('#bAdd');
 const itTask = document.querySelector('#itTask');
 const form = document.querySelector('#bAdd');
 
+// una vez que se ingresa al arreglo
 form.addEventListener('submit',e =>{
     e.preventDefault();
     if (itTask.value != ''){
         createTask(itTask.value);
         itTask.value ='';
+        rederTask();
     };
 });
 //contiene elementeos y objetos
 function createTask(value){
-    const newTask ="";
+    //genera 3 caracteres
+    const newTask ={
+        id:(Math.random()*100).toString(36).slice(3),
+        title : value,
+        completed:falsed
+    };
+    task.unshift(newTask);
 }
+
+// toma los elementos y genera un html y inyecta a un conetdeor 
+function rederTask(){
+    const html= task.map((task => {
+        retunr `
+        <div class= "task>
+            <div class = "completed"></div>
+            <div class = "completed"></div>
+        </div>
+    `;
+
+    });
